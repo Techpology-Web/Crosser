cca72ea2ed08e576c22df247c1eaa5e9ae1b0092
spynetS <you@example.com>
2023-09-08 02:46:33 +0200

    update

--- backend/identification/models.py
+++ backend/identification/models.py
@@ -9,9 +9,20 @@ class User(models.Model):
     password = models.TextField()
 
     hashes = models.ManyToManyField(to=Hash) # hashes to verify files
+    onetime_hashes = models.ManyToManyField(to=Hash) # hashes to verify files can only be used once
 
     decompressed_size = models.PositiveIntegerField(default=0) # real size
     compressed_size = models.PositiveIntegerField(default=0)   # compressed size
+
+    def unlock_file(self, hash):
+        """
+            if hash is inside hash return true
+            is hash is inside onetime_hash return true and remove the hash
+        """
+        pass
+    
+    
+
 
 
 class Session(models.Model):
