43541a43537890ce294e036a2a47ec590e11b84b
Alfred Roos <alfred@stensatter.se>
2023-11-12 20:52:41 +0100

    added dynamic baseUrl

--- frontend/src/axiost.js
+++ frontend/src/axiost.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
 import {getCookie} from "./CookieHandler.js"
 
+let path = window.location.hostname;
+
+let baseUrl = "http://localhost:8000"
+if (path === "www.techpology.com") {
+  baseUrl = "https://cross.techpology.com";
+} else {
+  baseUrl = "http://localhost:8000";
+}
+
 const instance = axios.create({
   //baseURL: 'http://localhost:8000', // Replace with your API base URL
-  baseURL: 'https://cross.techpology.com', // Replace with your API base URL
+  baseURL: baseUrl, // Replace with your API base URL
   headers: {
     'sessionKey': getCookie("sessionKey"),
     // Add any other default headers you need here
